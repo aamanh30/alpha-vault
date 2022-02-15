f5cecf5e5989676796cbcc5141cba358e9ef0823
aamanh30 <aamanh30@gmail.com>
2022-02-16 01:26:13 +0530

    Added the necessary files for Profile Module

--- src/app/features/profile/profile-routing.module.ts
+++ src/app/features/profile/profile-routing.module.ts
@@ -1,16 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ProfilePageComponent } from './pages/profile-page/profile-page.component';
 
-const routes: Routes = [
-  {
-    path: '',
-    component: ProfilePageComponent
-  }
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class ProfileRoutingModule {}
+export class ProfileRoutingModule { }
--- src/app/features/profile/profile.module.ts
+++ src/app/features/profile/profile.module.ts
@@ -2,13 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ProfileRoutingModule } from './profile-routing.module';
-import { ProfilePageComponent } from './pages/profile-page/profile-page.component';
 
 
 @NgModule({
-  declarations: [
-    ProfilePageComponent
-  ],
+  declarations: [],
   imports: [
     CommonModule,
     ProfileRoutingModule
