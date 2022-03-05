bad9d6ee15c37f50403cd9a64606c6669d8af8ae
aamanh30 <aamanh30@gmail.com>
2022-03-06 01:58:29 +0530

    Updated the routing with guard

--- src/app/features/home/home-routing.module.ts
+++ src/app/features/home/home-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LoggedInGuard } from './guards/logged-in.guard';
+import { LoggedInGuard } from './guards/logged-in/logged-in.guard';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { MainPageComponent } from './pages/main-page/main-page.component';
 
